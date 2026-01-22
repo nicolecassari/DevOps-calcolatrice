919eddcd9def0357890f9df941df021ba473ac50
Nic♡le <cassarinicole@gmail.com>
2026-01-22 16:43:18 +0100

    si

--- calcolatrice.py
+++ calcolatrice.py
@@ -10,15 +10,18 @@ def calcolatrice (operazione: str, a: float,b: float):
         return None
     
 if __name__=="__main__":
-   # print(
-
-        # "Somma di 3 e 5:", calcolatrice("somma", 3,5), "\n",
-       #  "Sottrazione di 10 a 7:", calcolatrice("sottrazione", 10,7), "\n",
-       #  "Moltiplicazione di 3 e 5:", calcolatrice("moltiplicazione", 3,5)
-   # )
-
+    
     x = input("Inserisci il primo numero: ")
     y = input("Inserisci il secondo numero: ")
     print("Somma di", x, "e", y, ":", calcolatrice("somma", float(x), float(y)))
     print("Sottrazione di", x, "e", y, ":", calcolatrice("sottrazione", float(x), float(y)))
     print("Moltiplicazione di", x, "e", y, ":", calcolatrice("moltiplicazione", float(x), float(y)))
+
+
+
+    # print(
+
+        # "Somma di 3 e 5:", calcolatrice("somma", 3,5), "\n",
+       #  "Sottrazione di 10 a 7:", calcolatrice("sottrazione", 10,7), "\n",
+       #  "Moltiplicazione di 3 e 5:", calcolatrice("moltiplicazione", 3,5)
+   # )
